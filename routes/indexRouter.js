55822da177794ed1ab8c3cf54a586b636f0ba3bc
Vishwanauth Persaud <vpersaud1245@gmail.com>
2024-08-10 16:19:08 -0400

    feat: Add index router get method

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const indexController = require("../controllers/indexController");
+
+router.get("/", indexController.get);
 
 module.exports = router;
