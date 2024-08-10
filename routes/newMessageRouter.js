9b55b60bebbaf65a58c6005f16a7ca0ab21dc62b
Vishwanauth Persaud <vpersaud1245@gmail.com>
2024-08-10 16:38:55 -0400

    feat: Add new message get route

--- routes/newMessageRouter.js
+++ routes/newMessageRouter.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const newMessageController = require("../controllers/newMessageController");
+
+router.get("/", newMessageController.get);
 
 module.exports = router;
