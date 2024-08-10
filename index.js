b5ebf4c88c3e7d71cfce77293b5b704f3375ba77
Vishwanauth Persaud <vpersaud1245@gmail.com>
2024-08-10 16:18:01 -0400

    feat: Configure ejs path and view engine

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const PORT = 3000;
 const indexRouter = require("./routes/indexRouter");
 const newMessageRouter = require("./routes/newMessageRouter");
 
+const path = require("node:path");
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
+
 app.use("/", indexRouter);
 app.use("/new", newMessageRouter);
 
