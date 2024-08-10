3136efd20e26c95ebb27333c1de21ac9d8bf67b1
Vishwanauth Persaud <vpersaud1245@gmail.com>
2024-08-10 16:49:34 -0400

    feat: Add new message form post controller

--- controllers/newMessageController.js
+++ controllers/newMessageController.js
@@ -1,5 +1,14 @@
+const messages = require("../models/messages");
 module.exports = {
   get: (req, res) => {
     res.render("form");
+  },
+  post: (req, res) => {
+    messages.push({
+      text: req.body.message,
+      user: req.body.authorsName,
+      added: new Date(),
+    });
+    res.render("index", { messages: messages });
   },
 };
