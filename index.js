96028ae1bf8f8ddbc3da7ac8b9afc0a63703db5d
Vishwanauth Persaud <vpersaud1245@gmail.com>
2024-08-10 16:49:15 -0400

    feat: Add url encoded to read form req body

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const PORT = 3000;
 const indexRouter = require("./routes/indexRouter");
 const newMessageRouter = require("./routes/newMessageRouter");
 
+app.use(express.urlencoded({ extended: true }));
+
 const path = require("node:path");
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
