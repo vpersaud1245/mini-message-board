e2cef6bc6de7ab11683dd7f7c4bf970097476e47
Vishwanauth Persaud <vpersaud1245@gmail.com>
2024-08-16 14:21:26 -0400

    feat: Add env PORT

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const indexRouter = require("./routes/indexRouter");
 const newMessageRouter = require("./routes/newMessageRouter");
 const messageRouter = require("./routes/messageRouter");
